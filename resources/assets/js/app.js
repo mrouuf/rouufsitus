8d72e9446d40b8ff7411204b02caf1d8bd05bc4f
unknown <syihaabrouuf@gmail.com>
2018-10-15 06:20:48 +0700

    Initial Commit -Laravel Installed

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy'
+
+Vue.use(Buefy)
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+var app = new Vue({
+	el: '#app',
+	data: {}
+})
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar has-shadow" >
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item is-paddingless brand-item" href="{{route('home')}}">
+        <img src="{{asset('images/fix-logo.png')}}" alt="Situs Logo">
+      </a>
+
+      @if (Request::segment(1) == "manage")
+        <a class="navbar-item is-hidden-desktop" id="admin-slideout-button">
+          <span class="icon">
+            <i class="fa fa-arrow-circle-right"></i>
+          </span>
+        </a>
+      @endif
+
+      <button class="button navbar-burger">
+       <span></span>
+       <span></span>
+       <span></span>
+     </button>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item is-tab is-active m-l-10">Learn</a>
+        <a class="navbar-item is-tab m-l-10">Discuss</a>
+        <a class="navbar-item is-tab m-l-10">Share</a>
+      </div> <!-- end of .navbar-start -->
+
+
+      <div class="navbar-end nav-menu" style="overflow: visible">
+        @if (!Auth::guest())
+          <a href="#" class="navbar-item is-tab">Login</a>
+          <a href="#" class="navbar-item is-tab">Join the Community</a>
+        @else
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">Hey Rouuf!</a>
+            <div class="navbar-dropdown is-right" >
+              <a href="#" class="navbar-item">
+                <span class="icon">
+                  <i class="fa fa-fw fa-user-circle-o m-r-15"></i>
+                </span>Profile
+              </a>
+
+              <a href="#" class="navbar-item">
+                <span class="icon">
+                  <i class="fa fa-fw fa-bell m-r-15"></i>
+                </span>Notifications
+              </a>
+              <a href="#" class="navbar-item">
+                <span class="icon">
+                  <i class="fa fa-fw fa-cog m-r-15"></i>
+                </span>Manage
+              </a>
+              <hr class="navbar-divider">
+              <a href="{{route('logout')}}" class="navbar-item" onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+                <span class="icon">
+                  <i class="fa fa-fw fa-sign-out m-r-15"></i>
+                </span>
+                Logout
+              </a>
+            </div>
+          </div>
+        @endif
+      </div>
+    </div>
+
+  </div>
+</nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
