ba43a503cdeef243de198d138863e721f70a450f
unknown <syihaabrouuf@gmail.com>
2018-10-18 15:46:22 +0700

    configuring laratrust

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,13 +75,15 @@
                   <i class="fa fa-fw fa-cog m-r-15"></i>
                 </span>Manage
               </a>
-              <hr class="navbar-divider">
-              <a href="{{route('logout')}}" class="navbar-item" onclick="event.preventDefault();
-                         document.getElementById('logout-form').submit();">
+              <hr class="navbar-divider">              
+              <a href="{{ route('logout') }}" class="navbar-item" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
                 <span class="icon">
                   <i class="fa fa-fw fa-sign-out m-r-15"></i>
                 </span>
                 Logout
+                <form id="frm-logout" action="{{ route('logout') }}"  method="POST" style="display: none;">
+              {{ csrf_field() }}
+              </form>
               </a>
             </div>
           </div>
